4ec23bcd9a76ef0a4480c3825a5e1d0f75c15c52
guy.b@heilasystems.com <guy.b@heilasystems.com>
2020-01-22 01:58:34 +0200

    Step 3 : making sure the program does not exit

--- fesource/hello.go
+++ fesource/hello.go
@@ -5,9 +5,16 @@ import (
 	"syscall/js"
 )
 
+var killSignal chan bool
+
+func init() {
+	killSignal = make(chan bool)
+}
+
 func main() {
 	fmt.Println("Hello, WASM !!")
 	js.Global().Set("helloAgain", js.FuncOf(helloAgain))
+	<-killSignal
 }
 
 func helloAgain(this js.Value, inputs []js.Value) interface{} {

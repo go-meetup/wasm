1c54ffc1705e9084945586e5ac6f605c62891f90
guy.b@heilasystems.com <guy.b@heilasystems.com>
2020-01-22 01:53:49 +0200

    Step 2 : call a go func from js (broken)

--- fesource/hello.go
+++ fesource/hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
 	fmt.Println("Hello, WASM !!")
+	js.Global().Set("helloAgain", js.FuncOf(helloAgain))
+}
+
+func helloAgain(this js.Value, inputs []js.Value) interface{} {
+	fmt.Println("Hello again, WASM !!")
+	return nil
 }
